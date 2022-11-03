01d8a8ffdec07627b69c9703e5c7e21928d11c67
dimaska <parshindi@mail.ru>
2022-11-03 16:36:30 +0200

    add migrations

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path(
         'redoc/',
